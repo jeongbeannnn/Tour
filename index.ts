d3703be578ef169114a4982fab18f7ae32409bd9
conoson <kos982000@gmail.com>
2024-05-01 19:29:44 +0900

    file root 변경

--- index.ts
+++ index.ts
@@ -1,14 +1,7 @@
-var http = require('http'),
-    fs = require('fs');
+Bun.serve({
+  fetch(req: Request): Response | Promise<Response> {
+    return new Response(Bun.file("src/announcement/Announcement.html"));
+  },
 
-
-fs.readFile('./index.html', function (err, html) {
-    if (err) {
-        throw err; 
-    }       
-    http.createServer(function(request, response) {  
-        response.writeHeader(200, {"Content-Type": "text/html"});  
-        response.write(html);  
-        response.end();  
-    }).listen(8000);
+  port: process.env.PORT || 3000,
 });
